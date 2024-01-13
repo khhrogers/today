696b1c9c43c7cb6859ea9f0e1815efaa3f5ca3a6
sweetdough <khhrogers@gmail.com>
2024-01-13 17:04:21 -0500

    changed to showing universities in america becaus

--- mynews/src/App.js
+++ mynews/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
   
-   
-  return (
-   <div>
-    <h1> Top News </h1>
-   </div>
+  function App() {
+
+    // hold the data from site 
+    const [universities, setUniversities] = useState([]);
+  
+    useEffect(() => {
+      fetch('http://universities.hipolabs.com/search?country=United+States')
+        .then(response => response.json())
+        .then(data => setUniversities(data))
+        .catch(error => console.error('Error fetching data: ', error));
+    }, []);
+  
+    return (
+
+      // return the name of the university and web page by mapping through the data 
+      <div>
+        <h1 className='title'>Universities in the United States</h1>
+       <ul>
+        {universities.map((university, index) => (
+          <li key={index}>{university.name} - <a href={university.web_pages[0]} target="_blank" rel="noopener noreferrer">Website</a></li>
+        ))}
+</ul>
+      </div>
   );
 }
 
